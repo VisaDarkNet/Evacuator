8a76b61a47f112b41d74347e8af3c0c64faa679b
visadarknet <visadarknet@gmail.com>
2024-09-01 10:47:18 +0500

    Редактирование файлов

--- app/js/main.js
+++ app/js/main.js
@@ -6,16 +6,7 @@ $(document).ready(function () {
     slidesToScroll: 2,
     dots: true,
     prevArrow: '<button type="button" class="species__btn slick-prev"><img class="species__icons" src="images/icons/arrow-right.svg" alt=""></button>',
-    nextArrow: '<button type="button" class="species__btn slick-next"><img class="species__icons" src="images/icons/arrow-right.svg" alt=""></button>',
-    responsive: [
-      {
-        breakpoint: 993,
-        settings: {
-          dots: true,
-          arrows: false
-        }
-      }
-    ]
+    nextArrow: '<button type="button" class="species__btn slick-next"><img class="species__icons" src="images/icons/arrow-right.svg" alt=""></button>'
   });
 
 });
@@ -28,5 +19,15 @@ $('.product-tabs__info-item').on('click', function (e) {
 
   $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
   $($(this).attr('href')).addClass('product-tabs__content-item--active')
+
+});
+
+$(document).ready(function () {
+  $(".menu__link,.header__link,.arrow-top").on("click", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top - 0;
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
 
 });
